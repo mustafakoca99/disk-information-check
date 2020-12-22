8af592ac393525f73fea76ff705ee6ebdaaeb1aa
Mustafa Koca <54147948+mustafakoca99@users.noreply.github.com>
2020-12-22 23:00:10 +0300

    DiskInformationCheck
    Provides disk information. You can say folder and file manager. It is the system that shows the free space on the disks, used space etc.

---
+++ dosyavedizinkontrol/dosyavedizinkontrol/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace dosyavedizinkontrol
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 29);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(271, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "sürücü";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(15, 56);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(268, 355);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.ListBox1_MouseDoubleClick);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(289, 56);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(499, 355);
+            this.listBox2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
+
---
+++ dosyavedizinkontrol/dosyavedizinkontrol/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace dosyavedizinkontrol
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public DriveInfo[] suruculer;
+        public DirectoryInfo suankikonum;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            suruculer = DriveInfo.GetDrives();
+            foreach (DriveInfo item in suruculer.Where(s=>s.IsReady))
+            {
+                comboBox1.Items.Add(item.Name);
+            }
+            
+        }
+
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex>-1)
+            {
+                
+                DriveInfo bilgi =
+                    suruculer.Where((s => s.Name == comboBox1.SelectedItem.ToString())).SingleOrDefault();
+
+                double bosalan = bilgi.AvailableFreeSpace / (1024 * 1024 * 1024);
+                double toplamalan = bilgi.TotalSize / (1024 * 1024 * 1024);
+
+                toolStripStatusLabel1.Text = string.Format("{0}, {1} - boş alan: {2} gb - toplam {3} gb",
+                    bilgi.Name, bilgi.VolumeLabel, bosalan, toplamalan
+                    );
+
+                suankikonum = bilgi.RootDirectory;
+
+                dizinleriguncelle();
+            }
+        }
+
+        private void dizinleriguncelle()
+        {
+            listBox1.Items.Clear();
+            listBox1.Items.Add("...");
+            DirectoryInfo[] dizinler = suankikonum.GetDirectories();
+            foreach  (DirectoryInfo dizin in dizinler)
+            {
+                listBox1.Items.Add(dizin.Name);
+            }
+
+            listBox2.Items.Clear();
+            FileInfo[] dosyalar = suankikonum.GetFiles();
+            foreach (FileInfo item in dosyalar)
+            {
+                listBox2.Items.Add(item.Name);
+            }
+        }
+
+        private void ListBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            try
+            {
+                if (listBox1.SelectedIndex > -1)
+            {
+                string secilen_ismi = listBox1.SelectedItem.ToString();
+                
+                    if (secilen_ismi == "...")
+                    {
+                        suankikonum = suankikonum.Parent;
+                        dizinleriguncelle();
+                        return;
+                    }
+                
+                suankikonum = suankikonum.GetDirectories().Where(
+                    s => s.Name == secilen_ismi
+                    ).SingleOrDefault();
+                //bunu koymazsan hata verir :)
+                dizinleriguncelle();
+            }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("en üst dizin burası!!");
+            }
+        }
+    }
+}
